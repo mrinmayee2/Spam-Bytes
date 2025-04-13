9a872d986f097c65f0176f2ac0be31782f10d3c9
Aishwaryae31 <162700186+Aishwaryae31@users.noreply.github.com>
2025-04-13 22:58:00 +0530

    threads.js

--- frontend/threads.js
+++ frontend/threads.js
@@ -1,12 +1,2 @@
-// (your Threads code starts here)
 
-function Threads(props) {
-    // your Threads component code...
-    // probably setting up WebGL, mouse interaction, etc.
-  }
-  
-  // (your Threads code ends here)
-  
-  // Add this one line:
-  window.Threads = Threads;
   
